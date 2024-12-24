8f18ef25bf0afbc9cbb7bf6213bae38edacce19f
AdrianOjeda <adrian.16300022@gmail.com>
2024-12-23 20:14:25 -0600

    get empleados endpoint done

--- src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -1,12 +1,16 @@
 package gm.rh.controlador;
 
+import gm.rh.modelo.Empleado;
 import gm.rh.servicio.EmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rh-app")
@@ -17,5 +21,10 @@ public class EmpleadoControlador {
     @Autowired
     private EmpleadoServicio empleadoServicio;
 
-
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        var empleados= empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado->logger.info(empleado.toString())));
+        return empleados;
+    }
 }
