acc5a8460665b6f8a4e08d68a368d8689364ed84
AdrianOjeda <adrian.16300022@gmail.com>
2025-01-15 20:56:18 -0600

    editar empleado

--- src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -44,4 +44,18 @@ public class EmpleadoControlador {
         return ResponseEntity.ok(empleado);
 
     }
+
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Integer id, @RequestBody Empleado empleadoRecibido) {
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if (empleado == null) {
+            throw new RecursoNoEncontradoExcepcion("El id no existe");
+        }
+        empleado.setNombre(empleadoRecibido.getNombre());
+        empleado.setDepartamento(empleadoRecibido.getDepartamento());
+        empleado.setSueldo(empleadoRecibido.getSueldo());
+        empleadoServicio.guardarEmpleado(empleado);
+        return ResponseEntity.ok(empleado);
+    }
+
 }
